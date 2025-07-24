510cc8e5675ab98aa84df5c60512a88ad352bb20
Paul L <paul.legoux@gmail.com>
2025-07-24 16:29:49 -0700

    fix: tweak config to support hot vite reload of components

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
+import path from "path"
 
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: "dist", // Change "dist" to your desired output directory
+    emptyOutDir: true,
+  },
   server: {
     open: "/index.html",
+  },
+
+  resolve: {
+    alias: {
+      "murmuring-boids": path.resolve(__dirname, "components/murmuring-boids.link"),
+    },
   },
 })
