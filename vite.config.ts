4411c75b975f8b41eb1ef407d34f441e22d15178
Paul L <paul.legoux@gmail.com>
2025-07-24 19:56:53 -0700

    draft: add sample for GitHub workflow that auto-publish updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: "dist", // Change "dist" to your desired output directory
+    outDir: "target",
     emptyOutDir: true,
   },
   server: {
