8eb0941436326b75e9fad593fc68d2dc00473ef5
Paul L <paul.legoux@gmail.com>
2025-09-08 22:13:36 -0700

    clean: make the scripts truly POSIX-compatible

--- .scripts/includes/logs.sh
+++ .scripts/includes/logs.sh
@@ -10,8 +10,7 @@ RED='\033[0;31m'
 DARKGRAY='\033[1;30m'
 
 
-function level_color()
-{
+level_color() {
     case "$1" in
         INFO)    echo "$BLUE";;
         WARNING) echo "$ORANGE";;
@@ -22,33 +21,36 @@ function level_color()
 }
 
 # == Functions
-function log_info()    { log "INFO"    "$@"; }
-function log_warning() { log "WARNING" "$@"; }
-function log_error()   { log "ERROR"   "$@"; }
-function log_success() { log "SUCCESS" "$@"; }
+log_info()    { log "INFO"    "$@"; }
+log_warning() { log "WARNING" "$@"; }
+log_error()   { log "ERROR"   "$@"; }
+log_success() { log "SUCCESS" "$@"; }
 
 
-function log()
-{
-    local level="$1"
-    local message_summary="$2"
-    local message_details="${@:3}"
+log() {
+    (
+        set -eu
 
-    local color=$(level_color "$level")
-    if [ -z "$color" ]; then
-        log "ERROR" "Undefined log level: $level"
-        return 1
-    fi
-    local padding=" "
-    local max_padding=8  # max_length_log_level + 1
-    local padding_length=$(($max_padding - ${#level}))
+        level="${1:-}"
+        message_summary="${2:-}"
+        message_details="${@:3}"
 
-    printf "[$color${level}$NC]%${padding_length}s${message_summary}\n" "$padding"
-    if [ -z "$message_details" ]; then
-        return
-    fi
-    for line in "$message_details"; do
-        # (max_padding + 2) to take square brackets [] into account
-        printf "%$(($max_padding + 2))s${line}\n" "$padding"
-    done
+        color=$(level_color "$level")
+        if [ -z "$color" ]; then
+            log "ERROR" "Undefined log level: $level"
+            return 1
+        fi
+        padding=" "
+        max_padding=8  # max_length_log_level + 1
+        padding_length=$(($max_padding - ${#level}))
+
+        printf "[$color${level}$NC]%${padding_length}s${message_summary}\n" "$padding"
+        if [ -z "$message_details" ]; then
+            return
+        fi
+        for line in "$message_details"; do
+            # (max_padding + 2) to take square brackets [] into account
+            printf "%$(($max_padding + 2))s${line}\n" "$padding"
+        done
+    )
 }
--- .scripts/includes/utils.sh
+++ .scripts/includes/utils.sh
@@ -4,32 +4,35 @@
 # Inputs:
 #   $1: Name of shell package to check. Version is obtained using '--version' option
 #   $2: Minimal version for that package
-function more_recent_version()
-{
-    local package=$1
-    local min_version=$2
-    # Verify the package exists
-    local base_message="Required: $package must be installed with version >= $min_version"
+more_recent_version() {
+    (
+        set -eu
 
-    local package_version=$(
-        $package --version 2>/dev/null | head -1 | grep -oE "[0-9]+\.[0-9]+(\.[0-9]+)?" | head -1 \
-    )
-    if [ -z "$package_version" ]; then
-        echo "$base_message, but was not found or could not read version" >&2
-        return 1
-    fi
+        package=$1
+        min_version=$2
+        # Verify the package exists
+        base_message="Required: $package must be installed with version >= $min_version"
 
-    # Verify the package version is greater or equal than the minimal version
-    # using version sort
-    if [ "$package_version" = "$min_version" ]; then
+        package_version=$(
+            $package --version 2>/dev/null | head -1 | grep -oE "[0-9]+\.[0-9]+(\.[0-9]+)?" | head -1 \
+        )
+        if [ -z "$package_version" ]; then
+            echo "$base_message, but was not found or could not read version" >&2
+            return 1
+        fi
+
+        # Verify the package version is greater or equal than the minimal version
+        # using version sort
+        if [ "$package_version" = "$min_version" ]; then
+            return 0  # success
+        fi
+        older_version=$(
+            printf "%s\n" "$package_version" "$min_version" | sort -V | head -1 \
+        )
+        if [ "$package_version" = "$older_version" ]; then
+            echo "$base_message, but found version $package_version for $(which $package)" >&2
+            return 1
+        fi
         return 0  # success
-    fi
-    local older_version=$(
-        printf "%s\n" "$package_version" "$min_version" | sort -V | head -1 \
     )
-    if [ "$package_version" = "$older_version" ]; then
-        echo "$base_message, but found version $package_version for $(which $package)" >&2
-        return 1
-    fi
-    return 0  # success
 }
